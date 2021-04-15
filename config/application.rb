13e131b33cb9c1da08b18f9745215b6c83e7bc0b
thanhdatdev <thanhdat.nguyen2104@gmail.com>
2021-04-16 05:02:51 +0700

    Precompile assets

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module OkgodaTravel
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
 
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
