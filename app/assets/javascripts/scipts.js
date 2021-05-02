e622ce4c603193d94c199c5e4199ebcfe0cedb93
thanhdatdev <thanhdat.nguyen2104@gmail.com>
2021-05-02 15:02:05 +0700

    refactor-coding-js-file

--- app/assets/javascripts/scipts.js
+++ app/assets/javascripts/scipts.js
@@ -15661,3 +15661,95 @@ function getCookie(cname) {
     });
 }
 )(jQuery, window, document);
+
+function formatnumber(what) {
+    var _value = what;
+    _value = _value.replace('.', '');
+    _value = _value.replace('.', '');
+    _value = _value.replace('.', '');
+    var temp = '';
+    var test = _value.length;
+    var count = 0;
+    for (i = test - 1; i >= 0; i--) {
+        count++
+        temp = _value.charAt(i) + temp;
+        if (count % 3 == 0 && i > 0) {
+            temp = ',' + temp;
+        }
+    }
+    what = temp;
+    return what;
+}
+function confirmDelete() {
+    var con = confirm("Bạn có muốn xóa dữ liệu này không?");
+    if (con == true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function keyRestrict(e, validchars) {
+    var key = ''
+      , keychar = '';
+    key = getKeyCode(e);
+    if (key == null)
+        return true;
+    keychar = String.fromCharCode(key);
+    keychar = keychar.toLowerCase();
+    validchars = '-+' + validchars.toLowerCase();
+    if (validchars.indexOf(keychar) != -1)
+        return true;
+    if (key == null || key == 0 || key == 8 || key == 9 || key == 13 || key == 27)
+        return true;
+    return false;
+}
+function getKeyCode(e) {
+    if (window.event)
+        return window.event.keyCode;
+    else if (e)
+        return e.which;
+    else
+        return null;
+}
+function funCheckInt(e) {
+    return keyRestrict(e, '0123456789');
+}
+function setAsHomePage(i) {
+    if (document.all) {
+        this.style.behavior = 'url(#default#homepage)';
+        this.setHomePage('http://www.travel.com.vn/');
+    }
+}
+var ent;
+function show_text(t, dname) {
+    var xp, yp, op
+    xp = dname.offsetLeft;
+    yp = dname.offsetTop;
+    while (dname.offsetParent) {
+        op = dname.offsetParent;
+        xp = xp + op.offsetLeft;
+        yp = yp + op.offsetTop;
+        dname = dname.offsetParent;
+    }
+    var newdiv = document.createElement('div');
+    newdiv.setAttribute('id', "ent");
+    document.body.appendChild(newdiv);
+    ent = document.getElementById("ent")
+    if (ent) {
+        ent.style.color = "#000000";
+        ent.style.padding = "7px 8px 7px 8px";
+        ent.style.background = "#eee";
+        ent.style.border = "1px solid #0066cb";
+        ent.style.position = 'absolute';
+        ent.style.left = (xp + 10) + "px";
+        ent.style.top = (yp + 25) + "px";
+        ent.innerHTML = t;
+        ent.style.display = "block";
+    }
+}
+function clear_text(dname) {
+    ent = document.getElementById("ent");
+    if (ent) {
+        document.body.removeChild(ent);
+    }
+}
