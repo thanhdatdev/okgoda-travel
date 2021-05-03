c92f720b22b7b43be0a2259842fc1fb0a57b3e74
thanhdatdev <thanhdat.nguyen2104@gmail.com>
2021-05-04 05:44:44 +0700

    refactor-mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,15 +89,16 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.default_url_options = { host: 'okgoda-travel-application.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'okgoda-travel-application.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => "gmail.com",
+    :domain               => "okgoda-travel-application.herokuapp.com",
     :user_name            => "zxzmakerzxz@gmail.com",
     :password             => "Dungyeuthai",
     :authentication       => "plain",
