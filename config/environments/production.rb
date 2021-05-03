6af21bcb1b48377852b9d02eb2d3e8870f07c06b
thanhdatdev <thanhdat.nguyen2104@gmail.com>
2021-05-04 05:31:12 +0700

    refactor-mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  host = 'smtp.gmail.com' #replace with your own url
-  config.action_mailer.default_url_options = { host: host }
+  # host = 'smtp.gmail.com' #replace with your own url
+  # config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
