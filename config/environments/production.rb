5feee677e4c3a956cbfdf2497722e1fcfe80c3d1
thanhdatdev <thanhdat.nguyen2104@gmail.com>
2021-05-04 03:55:53 +0700

    config send email on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,20 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://okgoda-travel-application.herokuapp.com/' #replace with your own url
+  config.action_mailer.default_url_options = { host: host }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => "zxzmakerzxz@gmail.com",
+    :password             => "Dungyeuthai",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
