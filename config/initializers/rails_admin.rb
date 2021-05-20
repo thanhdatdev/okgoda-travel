75633b79c22c725f163722a120a156ed7bc53c7d
thanhdatdev <thanhdat.nguyen2104@gmail.com>
2021-05-21 02:39:45 +0700

    abiity users

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -9,8 +9,10 @@ RailsAdmin.config do |config|
   # config.current_user_method(&:current_user)
 
   ## == CancanCan ==
-  # config.authorize_with :cancancan
-
+  config.authorize_with :cancancan
+  config.authorize_with do
+      redirect_to main_app.root_path unless current_user.admin === true
+    end
   ## == Pundit ==
   # config.authorize_with :pundit
 
