c91c47ffee409d3aad491df11dc4d75f53e624ee
ThanhDatNguyen <thanhdat.nguyen2104@gmail.com>
2021-05-30 03:26:36 +0700

    config send email errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   end
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'okgoda-travel-application.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'okgoda-travel-application.herokuapp.com/' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
 
@@ -104,7 +104,7 @@ Rails.application.configure do
     :user_name            => ENV["EMAIL_USER"],
     :password             => ENV["EMAIL_PASSWORD"],
     :authentication       => "plain",
-    :enable_starttls_auto => true
+    # :enable_starttls_auto => true
   }
 
   # Do not dump schema after migrations.
