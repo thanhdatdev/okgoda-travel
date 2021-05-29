863206257596f5cd406318f9a496d4bfe6a5b32b
ThanhDatNguyen <thanhdat.nguyen2104@gmail.com>
2021-05-29 20:00:18 +0700

    refactor coding handle errors 404

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,4 @@
 class ErrorsController < ApplicationController
-  unless Rails.application.config.consider_all_requests_local
-    rescue_from ActionController::RoutingError, with: :render_canvas_404
-    # rescue_from ActionController::UnknownController, with: :render_404
-    rescue_from AbstractController::ActionNotFound, with: :render_404
-    rescue_from ActiveRecord::RecordNotFound, with: :render_404
-    rescue_from ActiveResource::ForbiddenAccess, with: :render_403
-    rescue_from CanCan::AccessDenied, with: :render_403
-  end
-
   def render_404
     render status: 404
   end
