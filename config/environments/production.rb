9f306dadfe39f0f85914be5cf3bcc74d2bf9acbc
ThanhDatNguyen <thanhdat.nguyen2104@gmail.com>
2021-05-30 03:27:16 +0700

    config send email errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   end
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'okgoda-travel-application.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'okgoda-travel-application.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
 
